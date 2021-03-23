7982b82970a71e945b4f16e312c4f7623161156c
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-23 18:24:10 -0300

    exercises: part 1

---
+++ fundamentos/bloco_7/dia_1/part_1.js
@@ -0,0 +1,26 @@
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(false);
+
+// =======================
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+oddsAndEvens.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(
+  `Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`
+);
